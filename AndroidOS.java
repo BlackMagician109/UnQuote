f4d2ba51a1668d2a2b66e6ae28ecc7d607373b74
BlackMagician109 <96339439+BlackMagician109@users.noreply.github.com>
2023-01-19 03:30:05 -0600

    Completed the Question class.

--- AndroidOS.java
+++ AndroidOS.java
@@ -6,7 +6,10 @@ public class AndroidOS {
     public void runQuizApp() {
         // Create three questions here
 
-        // System.out.println("Question 1: "" + ...)
+        Question q1 = new Question(1, 921238, "How tall is the Eiffel tower?", "1024 ft", "1063 ft", "1124 ft", "1163 ft");
+        Question q2 = new Question(3, 107343, "Who invented the computer algorithm?", "Charles Babbage", "John Carmack", "Alan Turing", "Ada Lovelace");
+        Question q3 = new Question(2, 748294, "What is the name for the patch of skin found on your elbow?", "Elbow Skin", "Fascia Elbora", "Wenis", "Todd");
+
     }
 
     public static void main(String[] args) {
--- Question.java
+++ Question.java
@@ -1,3 +1,26 @@
 public class Question {
+    //Instance Variable
+    private int correctAns; //Correct answer to the question
+    private int userAns;    //Answer that the user entered
+    private int imageId;    //Stores the identifier of the image
+
+    private String questionText;    //The question text
+    private String ans0;    //Option zero for the question
+    private String ans1;    //Option one for the question
+    private String ans2;    //Option two for the quesion
+    private String ans3;    //Option three for the quesion
+
+    //Constructor
+    public Question(int correctAns, int imageId, String questionText, String ans0, String ans1, String ans2, String ans3){
+        this.correctAns = correctAns;
+        this.imageId = imageId;
+        this.questionText = questionText;
+        this.ans0 = ans0;
+        this.ans1 = ans1;
+        this.ans2 = ans2;
+        this.ans3 = ans3;
+        this.userAns = -1;
+    }
+
     
 }
